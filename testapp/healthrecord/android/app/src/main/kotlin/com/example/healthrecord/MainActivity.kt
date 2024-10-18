e14f29972662f645434dad1a56220676730bb100
rachelgjippy <racheljippy@gmail.com>
2024-10-18 07:00:32 +0000

    practiceapp

---
+++ testapp/healthrecord/android/app/src/main/kotlin/com/example/healthrecord/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.healthrecord
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ testapp/healthtracker/android/app/src/main/kotlin/com/example/healthtracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.healthtracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
