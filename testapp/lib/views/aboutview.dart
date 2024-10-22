5a2e0e1243ddc369f7ccac8a30629ad653ea1559
rachelgjippy <racheljippy@gmail.com>
2024-10-22 07:28:14 +0000

    Aboutview

---
+++ testapp/lib/views/aboutview.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Aboutview extends StatelessWidget {
+  const Aboutview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+  body: Text("My First Flutter App")
+    );
+  }
+}
