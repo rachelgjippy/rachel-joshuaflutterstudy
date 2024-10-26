f23c0d9b5840de51b61b5111567bd5643d8b0563
rachelgjippy <racheljippy@gmail.com>
2024-10-26 21:40:46 +0000

    Aboutview

--- testapp/healthrecord/android/app/src/main/kotlin/com/example/healthrecord/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.healthrecord
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- testapp/lib/views/aboutview.dart
+++ testapp/lib/views/aboutview.dart
@@ -7,6 +7,7 @@ class Aboutview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
   body: Text("My First Flutter App")
+
     );
   }
 }
