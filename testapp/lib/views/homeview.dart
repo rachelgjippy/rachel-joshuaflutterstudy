17da015a8d6653f0d5696c262f2bc90795773bb7
joshuawallace25 <131925695+joshuawallace25@users.noreply.github.com>
2024-10-29 15:16:28 -0700

    text Widget

--- testapp/lib/views/homeview.dart
+++ testapp/lib/views/homeview.dart
@@ -5,9 +5,7 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-body:  Text("Hello User")
-
-    );
+    return const Scaffold(
+        body: Text("Hello User", style: TextStyle(color: Colors.blue, fontSize: 20)));
   }
 }
